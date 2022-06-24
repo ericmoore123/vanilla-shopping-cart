55cc81d3bd84d52fccaccf71ae8a0825f90481de
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-06-23 22:42:31 -0400

    Styles.css file fixes

--- src/main.js
+++ src/main.js
@@ -1,34 +1,4 @@
 const shop = document.querySelector("#shop");
-// let productData = [
-//     {
-//       id: "jfhgbvnscs",
-//       name: "Casual Shirt",
-//       price: 45,
-//       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-//       img: "images/img-1.jpg",
-//     },
-//     {
-//       id: "ioytrhndcv",
-//       name: "Office Shirt",
-//       price: 100,
-//       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-//       img: "images/img-2.jpg",
-//     },
-//     {
-//       id: "wuefbncxbsn",
-//       name: "T Shirt",
-//       price: 25,
-//       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-//       img: "images/img-3.jpg",
-//     },
-//     {
-//       id: "thyfhcbcv",
-//       name: "Mens Suit",
-//       price: 300,
-//       desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-//       img: "images/img-4.jpg",
-//     },
-//   ];
 
 const generateShop = () => {
   return (shop.innerHTML = productData
@@ -42,11 +12,11 @@ const generateShop = () => {
             <div class="price-quantity">
                 <h2>${price}</h2>
                 <div class="btns">
-                <i onclick="decrement()" class="bi bi-dash-lg"></i>
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                 <div id=${id} class="quantity">
                     0
                 </div>
-                <i onclick="increment()" class="bi bi-plus-lg"></i>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
@@ -58,12 +28,12 @@ const generateShop = () => {
 
 generateShop();
 
-const increment = () => {
-  console.log("Increment");
+const increment = (id) => {
+  console.log(id);
 };
 
-const decrement = () => {
-  console.log("Decrement");
+const decrement = (id) => {
+  console.log(id);
 };
 
 const update = () => {};
